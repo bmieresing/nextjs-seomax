0f4f14bb17874515ae52f0e97e723d563133c4da
benjamin <benja037@gmail.com>
2024-05-29 15:37:06 -0400

    Try change helpers getURL

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,27 +1,13 @@
 
 
 export const getURL = (path = '') => {
-    // Verifica si NEXT_PUBLIC_SITE_URL está configurada y no está vacía.
-    let url =
-        process?.env?.NEXT_PUBLIC_SITE_URL &&
-        process.env.NEXT_PUBLIC_SITE_URL.trim() !== ''
-            ? process.env.NEXT_PUBLIC_SITE_URL
-            : // Si no está configurada, verifica NEXT_PUBLIC_VERCEL_URL, que es configurada automáticamente por Vercel.
-            process?.env?.NEXT_PUBLIC_VERCEL_URL &&
-            process.env.NEXT_PUBLIC_VERCEL_URL.trim() !== ''
-                ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-                : // Si ninguna está configurada, por defecto usa localhost para desarrollo local.
-                'http://localhost:3000';
+    const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000';
 
     // Elimina el slash final si existe.
-    url = url.replace(/\/+$/, '');
-    // Asegúrate de incluir `https://` cuando no sea localhost.
-    url = url.includes('http') ? url : `https://${url}`;
+    const url = baseUrl.replace(/\/+$/, '');
     // Asegúrate de que el path no tenga un slash inicial para evitar dobles slashes en la URL final.
-    path = path.replace(/^\/+/, '');
+    const finalPath = path.replace(/^\/+/, '');
 
-    const finalUrl = path ? `${url}/${path}` : url;
-    console.log("Final URL:", finalUrl);
-
-    return finalUrl;
+    // Concatenar la URL y el path.
+    return `${url}/${finalPath}`;
 };
