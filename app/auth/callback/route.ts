7feb8aa22e7169fa206919597fa8c2d45009972d
benjamin <benja037@gmail.com>
2024-05-29 16:05:38 -0400

    Change callback

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,39 +1,38 @@
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
-import { type CookieOptions, createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+import { type CookieOptions, createServerClient } from '@supabase/ssr';
 
 export async function GET(request: Request) {
-    const { searchParams, origin } = new URL(request.url)
-    console.log("searchParams",searchParams,"origin",origin)
-    const code = searchParams.get('code')
-    // if "next" is in param, use it as the redirect URL
-    const next = searchParams.get('next') ?? '/'
+  const { searchParams } = new URL(request.url);
+  const code = searchParams.get('code');
+  // if "next" is in param, use it as the redirect URL
+  const next = searchParams.get('next') ?? '/';
 
-    if (code) {
-        const cookieStore = cookies()
-        const supabase = createServerClient(
-            process.env.NEXT_PUBLIC_SUPABASE_URL!,
-            process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-            {
-                cookies: {
-                    get(name: string) {
-                        return cookieStore.get(name)?.value
-                    },
-                    set(name: string, value: string, options: CookieOptions) {
-                        cookieStore.set({ name, value, ...options })
-                    },
-                    remove(name: string, options: CookieOptions) {
-                        cookieStore.delete({ name, ...options })
-                    },
-                },
-            }
-        )
-        const { error } = await supabase.auth.exchangeCodeForSession(code)
-        if (!error) {
-            return NextResponse.redirect(`${origin}${next}`)
-        }
+  if (code) {
+    const cookieStore = cookies();
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          get(name: string) {
+            return cookieStore.get(name)?.value;
+          },
+          set(name: string, value: string, options: CookieOptions) {
+            cookieStore.set({ name, value, ...options });
+          },
+          remove(name: string, options: CookieOptions) {
+            cookieStore.delete({ name, ...options });
+          },
+        },
+      }
+    );
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    if (!error) {
+      return NextResponse.redirect(`${process.env.NEXT_PUBLIC_SITE_URL}${next}`);
     }
+  }
 
-    // return the user to an error page with instructions
-    return NextResponse.redirect(`${origin}/login?message=Could not login with provider`)
+  // return the user to an error page with instructions
+  return NextResponse.redirect(`${process.env.NEXT_PUBLIC_SITE_URL}/auth/auth-code-error`);
 }
